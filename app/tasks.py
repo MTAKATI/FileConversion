6bfa66622c67ae38aac68d3e0c8542b32ab65361
MTAKATI <sandisomtakati7@gmail.com>
2026-07-30 19:07:49 +0200

    Connected backend to frontend the application is working well

--- app/tasks.py
+++ app/tasks.py
@@ -1,26 +1,17 @@
-# For Celery background tasks & Conversion logic inside background workers
-# @author: Sandiso Mtakati
-# @date: 27/07/2026
-
-from celery import Celery
+from app.celery_app import celery_app
 from app.pipeline.handlers import ValidationHandler, ExecutionHandler, CleanupHandler
 
-app = Celery("file_conversion_worker", broker="redis://localhost:6379/0")
-
-@app.task(name="tasks.convert_file")
+@celery_app.task(name="app.tasks.convert_file_task")
 def convert_file_task(input_path: str, output_path: str) -> dict:
-    # Build execution chain: Validation -> Execution -> Cleanup
     cleanup = CleanupHandler()
     execution = ExecutionHandler(next_handler=cleanup)
     pipeline = ValidationHandler(next_handler=execution)
 
-    # Context object pass through the pipeline
     context = {
         "input_path": input_path,
         "output_path": output_path
     }
 
-    # Run pipeline chain
     result = pipeline.handle(context)
 
     return {
